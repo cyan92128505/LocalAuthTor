8b7c7c39e7129306e9fc7ff7a3457da92598aa83
cyan92128505 <cyan92128505@gmail.com>
2019-04-26 16:49:10 +0800

    Import local_auth and shared_preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:local_auth/local_auth.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() => runApp(MyApp());
 
@@ -24,23 +27,43 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
   String _state = 'UNKNOWN';
+  String _lockKey = 'LOCK_KEY';
+  bool _lockState = false;
 
   @override
   void initState() {
     super.initState();
+    _getLock();
     WidgetsBinding.instance.addObserver(this);
   }
 
   @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
+  didChangeAppLifecycleState(AppLifecycleState state) async {
     switch (state) {
       case AppLifecycleState.paused:
         _state = 'PAUSED';
         print(_state);
+        if (_lockState == false) {
+          _lockState = true;
+          _setLock();
+        }
         break;
       case AppLifecycleState.resumed:
         _state = 'RESUMED';
         print(_state);
+        if (_lockState == true) {
+          bool _biometrics = await hasBiometrics();
+          if (_biometrics == true) {
+            LocalAuthentication localAuth = new LocalAuthentication();
+            await localAuth.authenticateWithBiometrics(
+              localizedReason: 'USE Biometrics',
+              useErrorDialogs: false,
+            );
+            print('USE Biometrics');
+          }
+          _lockState = false;
+          _setLock();
+        }
         break;
       case AppLifecycleState.inactive:
         _state = 'INACTIVE';
@@ -57,6 +80,31 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
     }
 
     setState(() {});
+  }
+
+  void _getLock() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    _lockState = prefs.getBool(_lockKey) ?? false;
+  }
+
+  void _setLock() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(_lockKey, _lockState);
+  }
+
+  Future<bool> hasBiometrics() async {
+    LocalAuthentication localAuth = new LocalAuthentication();
+    bool canCheck = await localAuth.canCheckBiometrics;
+    if (canCheck) {
+      List<BiometricType> availableBiometrics =
+          await localAuth.getAvailableBiometrics();
+      if (Platform.isIOS && availableBiometrics.contains(BiometricType.face)) {
+        return true;
+      } else if (availableBiometrics.contains(BiometricType.fingerprint)) {
+        return true;
+      }
+    }
+    return false;
   }
 
   @override
